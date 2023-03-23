de976171033441a4832b4458e831b8948331a73f
Rajadev <102867229+Tommylegen@users.noreply.github.com>
2023-03-23 11:45:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Rajadev'// yourname
 global.MONGO_DB_URI = "mongodb+srv://wabot:TxPe8TGxDk4g8ScG@cluster0.rxaxkyc.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "tommylegends2@gmail.com" //email
-global.email_password = "yjjybjyghyfjgtgh" //application password email
+global.your_email = "" //email
+global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
